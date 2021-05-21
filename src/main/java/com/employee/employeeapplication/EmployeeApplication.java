b0963000e38ae142b6c8d4f800df76d0eba23632
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-21 09:13:18 +0530

    ADDED : slf4j and logging pattern

--- src/main/java/com/employee/employeeapplication/EmployeeApplication.java
+++ src/main/java/com/employee/employeeapplication/EmployeeApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@Slf4j
 public class EmployeeApplication {
 
     public static void main(String[] args) {
