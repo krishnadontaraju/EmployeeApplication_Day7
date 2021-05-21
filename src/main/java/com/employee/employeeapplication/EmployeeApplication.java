8db72571dfd47ccf119a13e159a4141d3a8f191b
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-21 10:24:55 +0530

    ADDED : dev and production properties

--- src/main/java/com/employee/employeeapplication/EmployeeApplication.java
+++ src/main/java/com/employee/employeeapplication/EmployeeApplication.java
@@ -1,17 +1,26 @@
 package com.employee.employeeapplication;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.StaticApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @Slf4j
 public class EmployeeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeApplication.class, args);
+        org.springframework.context.ApplicationContext context = SpringApplication.run(EmployeeApplication.class, args);
+       
+		log.info("Eploye Payroll App Started in {} Environment" , ((ConfigurableApplicationContext) context).getEnvironment().getProperty("environment"));
+        
     }
 
 }
